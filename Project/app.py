9d1476f7d1578f1b85a6b7ce5fec057022d85c5b
Victoriabunmi72 <bhummhie97@gmail.com>
2023-07-01 03:42:44 +0100

    app.py

--- Project/app.py
+++ Project/app.py
@@ -3,16 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, Email, ValidationError
-from flask_migrate import Migrate
+from wtforms.validators import InputRequired, Email, Length, ValidationError
 from flask_bcrypt import Bcrypt
+from flask_mail import Message, Mail
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
+mail = Mail()
 
 
 login_manager = LoginManager()
@@ -27,16 +27,16 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False, unique=True)
+    username = db.Column(db.String(60), nullable=False, unique=True)
     email = db.Column(db.String(120), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[
                            InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
 
-    email = StringField(validators=[
+email = StringField(validators=[
                         InputRequired(), Email()], render_kw={"placeholder": "Email"})
 
     password = PasswordField(validators=[
@@ -55,9 +55,8 @@ class RegisterForm(FlaskForm):
 class LoginForm(FlaskForm):
     username = StringField(validators=[
                            InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    email = StringField(validators=[InputRequired(), Email()], render_kw={"placeholder": "Email"})
 
-    email = StringField(validators=[
-                        InputRequired(), Email()], render_kw={"placeholder": "Email"})
     password = PasswordField(validators=[
                              InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
 
@@ -77,9 +76,24 @@ def login():
         if user:
             if bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user)
+
+                # Send email notification
+                send_login_notification(user.email)
+
                 return redirect(url_for('dashboard'))
+            else:
+                flash('Invalid username or password', 'error')
+        else:
+            flash('User not found', 'error')
     return render_template('login.html', form=form)
 
+def send_login_notification(email):
+    # Create the email message
+    msg = Message('Login Notification', sender='your_email@example.com', recipients=[email])
+    msg.body = 'You have successfully logged in to your account.'
+
+    # Send the email
+    mail.send(msg)
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
@@ -98,12 +112,26 @@ def logout():
 def register():
     form = RegisterForm()
 
+
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
         new_user = User(username=form.username.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
+
+        # Send email notification
+        send_registration_notification(new_user.username, new_user.email)
+
+
         return redirect(url_for('login'))
+
+def send_registration_notification(username, email):
+    # Create the email message
+    msg = Message('Registration Notification', sender='your_email@example.com', recipients=[email])
+    msg.body = f'Thank you for registering, {username}! Your account has been successfully created.'
+
+    # Send the email
+    mail.send(msg)
 
     return render_template('register.html', form=form)
 
